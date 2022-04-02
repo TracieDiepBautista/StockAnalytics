7c11605ab5a51608d4b8a3753a88d3a1323297a3
Tracie B <tracienguyen2021@gmail.com>
2022-04-02 12:55:50 -0700

    3rd_com

--- plot.js
+++ plot.js
@@ -1,318 +1,116 @@
 
-// Add active class to current nav selected link
-var a = document.querySelectorAll(".navbar li a");
-for (var i = 0, length = a.length; i < length; i++) {
-  a[i].onclick = function() {
-    var b = document.querySelector(".navbar li.active");
-    if (b) b.classList.remove("active");
-    this.parentNode.classList.add('active');
-  };
-}
+// initial page (Home page)
+// FUNCTION 1, set init() for dashboard and do dropdown button
+function init() {
 
-// Color palette variables
-let dark = '#252525';
-let mid = '#888';
-let light = 'rgba(255, 255, 255, 0.4)';
-
-// Add scrollmagic controller
-let controller = new ScrollMagic.Controller();
-
-//------------------
-//TIMELINE 1
-//------------------
-
-// Add timeline
-let tl1 = anime.timeline({autoplay: false});
-
-// Add animations
-let s1a1 = {
-  targets: '#one .elem',
-  opacity: 1,
-  translateX: {
-    value: [250, 0],
-    duration: 800
-  },
-  rotate: {
-    value: [90, 0],
-    duration: 1200,
-    easing: 'easeInOutSine'
-  },
-  scale: {
-    value: [2, 1.5],
-    duration: 1100,
-    delay: 800,
-    easing: 'easeInOutQuart'
-  },
-  color: [light, dark],
-  duration: 800,
-  delay: 0,
-  easing: 'easeInOutSine'
-};
-
-let s1a2 = {
-  targets: '#one .elem .blocks > div',
-  backgroundColor: [light, dark],
-  borderRadius: function(el) { return anime.random(2, 10); },
-  delay: function(el) { return anime.random(0, 800); }
-};
-
-let s1a3 = {
-  targets: '#one .rectangle',
-  opacity: [0,1],
-  translateX: {
-    value: ['-100vw', '0vw'],
-    duration: 1500,
-  },
-  translateY: {
-    value: [-100, 0],
-    duration: 1500,
-  },
-  easing: 'easeInOutSine',
-  duration: 2000
-};
-
-// Add children
-tl1.add(s1a3).add(s1a1, '-=1600').add(s1a2, '-=1300');
-
-// Get section height
-let oneHeight = document.getElementById("one").clientHeight;
-console.log('oneHeight: ' + oneHeight);
-
-//------------------
-//SCENE 1
-//------------------
-
-//Add first scrollmagic scene
-let scene1 = new ScrollMagic.Scene({
-  triggerElement: "#one",
-  triggerHook: 0.5,
-  reverse: false
-})
-
-// Add debug indicators
-.addIndicators({
-  colorTrigger: "black",
-  colorStart: "blue",
-  colorEnd: "red",
-  indent: 10
-})
-
-// Trigger animation timeline
-.on("enter", function (event) {
-  tl1.play();
-})
+  // select dropdown menu 
+  var dropdown = d3.select("navbardrop");
   
-.addTo(controller);
+  // read the data 
+  d3.json("finance.json").then((data)=> {
+    console.log(data)
+  
+    // get the id data to the dropdwown menu
+    data.names.forEach(function(name) {
+        dropdown.append("option").text(name).property("value",name);
+    });
+    getPriceInfo("Finance")
+    tracie("Finance")
+  });
+  }
+  
+  init();
+  
+  // Function 4, to get the necessary data for demographic box
+  
+  function getPriceInfo(id) {
+  
+    // read the json file to get data
+        d3.json("finance.json").then((data)=> {
+  
+    // get the metadata info for the demographic panel
+          var closeprice = data.close;
+          console.log(closeprice)
+    
+          // filter meta data info by stockname | filter function return array of values (for loop in js)
+          var prices = closeprice.filter(meta => meta.id.toString() === id)[0];
+  
+          // select demographic panel to put data
+          var porfolioInfo = d3.select("#sample-metadata");
+            
+         // empty the box info panel each time before getting new stock info
+          porfolioInfo.html("");
+    
+         // grab the necessary porfolio data for the box and append the info to the panel
+         // `$...` is similar to f string in python,
 
+          Object.entries(result).forEach(([key,value]) => {
+            porfolioInfo.append("h5").text(`${key.toUpperCase()} : ${value}`);    
+            });
+        });
+    }
+  
+    // FUNCTION 3, use function to do the change event
+  //(user click and change the id with appropriate content box)
+  
+  function optionChanged(id) {
+    getPriceInfo(id);
+    tracie(id);
+  }
+  
+  // Find the values for charts
+  // Create the 1st function to get data set out and use further:
+  // & use d3 to call the data from json file and put them in a place we name "data" to use further
+  
+  // Function 5: to get id, sample_values, otu_ids, otu_labels of TOP 10
+  
+  function tracie(StockValue) {
+    // var selector = d3.select("#selDataset");
+      d3.json("bank.json").then((data) => {
+        // console.log(data)
+  
+        var closeprices = data.close
+        console.log(closeprices)
+  
+        var volumes = data.volume
+        console.log(volumes)
 
-
-//------------------
-//TIMELINE 2
-//------------------
-
-// Add timeline
-let tl2 = anime.timeline({autoplay: false});
-
-// Add animations
-let s2a1 = {
-  targets: '#two .elem img',
-  opacity: [0.3,1],
-  scale: [4,1],
-  duration: 1000,
-  delay: 0,
-  easing: 'easeInOutSine'
-};
-
-let s2a2 = {
-  targets: '#two .elem img',
-  scale: 1,
-  duration: 2000,
-};
-
-// Add children
-tl2.add(s2a1).add(s2a2);
-
-// Get section height
-let twoHeight = document.getElementById("two").clientHeight;
-console.log('twoHeight: ' + twoHeight);
-
-//------------------
-//SCENE 2
-//------------------
-
-//Add second scrollmagic scene
-let scene2 = new ScrollMagic.Scene({
-  triggerElement: "#two",
-  duration: 4500,
-  triggerHook: 0,
-})
-
-// Add debug indicators
-.addIndicators({
-  colorTrigger: "black",
-  colorStart: "blue",
-  colorEnd: "red",
-  indent: 10
-})
-
-// Trigger animation timeline
-//Use scroll position to play animation
-.on("progress", function (event) {
-  tl2.seek(tl2.duration * event.progress);
-})
-
-.setPin('#two')
-.addTo(controller);
-
-
-
-//------------------
-//TIMELINE 3
-//------------------
-
-// Add timeline
-let tl3 = anime.timeline({autoplay: false});
-
-// Add animations
-let s3a1 = {
-  targets: '#three h2',
-  opacity: 1,
-  scale: [4,1.5],
-  duration: 1000,
-  delay: 0,
-  easing: 'easeInOutSine'
-};
-
-// Add children
-tl3.add(s3a1);
-
-//------------------
-//TIMELINE 4
-//------------------
-
-// Add timeline
-let tl4 = anime.timeline({autoplay: false});
-
-// Add animations
-let s3a2 = {
-  targets: '#three .image',
-  opacity: [0,.5],
-  translateX: {
-    value: ['-100%', '0%'],
-    duration: 1500,
-  },
-  duration: 1000,
-  delay: 0,
-  easing: 'easeInOutSine'
-};
-
-// Add children
-tl4.add(s3a2);
-
-//------------------
-//SCENE 3
-//------------------
-
-//Add third scrollmagic scene
-let scene3 = new ScrollMagic.Scene({
-  triggerElement: "#three",
-  duration: 2000,
-  triggerHook: 0
-})
-
-// Add debug indicators
-.addIndicators({
-  colorTrigger: "black",
-  colorStart: "blue",
-  colorEnd: "red",
-  indent: 10
-})
-
-// Trigger animation timeline
-//Use scroll position to play animation
-.on("enter", function (event) {
-  tl3.play();
-})
-.on("progress", function (event) {
-  tl4.seek((tl4.duration * event.progress) * 3);
-})
-
-.setPin('#three')
-.addTo(controller);
-
-
-
-
-//------------------
-//TIMELINE 5
-//------------------
-
-// Add timeline
-let tl5 = anime.timeline({autoplay: false});
-
-// Add animations
-let s4a1 = {
-  targets: '#four h2',
-  opacity: 1,
-  scale: [1.5,3],
-  duration: 1000,
-  delay: 0,
-  easing: 'easeInOutSine'
-};
-
-// Add children
-tl5.add(s4a1);
-
-//------------------
-//TIMELINE 6
-//------------------
-
-// Add timeline
-let tl6 = anime.timeline({autoplay: false});
-
-// Add animations
-let s4a2 = {
-  targets: '#four .image',
-  opacity: [0,.5],
-  translateX: {
-    value: ['100%', '0%'],
-    duration: 1500,
-  },
-  duration: 1000,
-  delay: 0,
-  easing: 'easeInOutSine'
-};
-
-// Add children
-tl6.add(s4a2);
-
-//------------------
-//SCENE 4
-//------------------
-
-//Add third scrollmagic scene
-let scene4 = new ScrollMagic.Scene({
-  triggerElement: "#four",
-  duration: 2000,
-  triggerHook: 0
-})
-
-// Add debug indicators
-.addIndicators({
-  colorTrigger: "black",
-  colorStart: "blue",
-  colorEnd: "red",
-  indent: 10
-})
-
-// Trigger animation timeline
-//Use scroll position to play animation
-.on("enter", function (event) {
-  tl5.play();
-})
-.on("progress", function (event) {
-  tl6.seek((tl6.duration * event.progress) * 3);
-})
-
-.setPin('#four')
-.addTo(controller);
+        var timeseries = data.date
+        
+        var filteredprice = closeprice.filter(priceObj => priceObj.close == StockValue)[1]
+  
+        var filteredvolumes = volumes.filter(volumeObj => volumeObj.id == StockValue)[1]
+  
+     
+        var otu_id = filteredSample.otu_ids;
+        console.log(otu_id)
+        
+        var otu_labels = filteredSample.otu_labels;
+        console.log(otu_labels)
+        
+        var sample_value = filteredSample.sample_values;
+        console.log(sample_value)
+  
+        var yvalue = otu_id.slice(0,10).map(otu_id=> `OTU ${otu_id}`).reverse()
+  
+      // now create BAR CHART with above 3 arrays
+      // Create a function for plotting first: 
+      
+  // create a Trace for plotting:
+        var bar_values = [{
+            x : sample_value.slice(0,10).reverse(),
+            y : yvalue,
+            text: otu_labels.slice(0,10).reverse(),
+            type: "bar",
+            orientation: "h",
+            marker: {color: "lightsalmon"},
+        }]
+        
+      // define the bar layout format: 
+        var bar_format = {
+            title: "Top 10 Microbial Species in Belly Button",
+            // xaxis: "Bacteria Sample Values",
+            // yaxis: "OTU IDs"
+        }
+        
+        Plotly.newPlot("bar", bar_values, bar_format)
